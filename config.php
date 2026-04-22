143a839300433f2e5c0f5ab6bf22499ddf0146e3
moochanakarn-pixel < GitHub>
2026-04-22 23:44:39 +0700

    Move DB credentials to settings.local.php

--- config.php
+++ config.php
@@ -127,8 +127,8 @@ function getDbConfig()
         'host' => localSetting($local, 'db_host', '127.0.0.1'),
         'port' => localSetting($local, 'db_port', 3307),
         'name' => localSetting($local, 'db_name', 'ini76'),
-        'user' => 'root',
-        'pass' => base64_decode('cG9zcHduZXQ='),
+        'user' => localSetting($local, 'db_user', 'root'),
+        'pass' => localSetting($local, 'db_pass', ''),
     );
 
     $dbConfig = normalizeDbConfig($inlineConfig);
